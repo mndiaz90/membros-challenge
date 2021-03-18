99d04f3c7efb8e7da7045c6533e2cc6cabe3fcd2
mndiaz90 <mayler900123@gmail.com>
2021-03-17 23:45:24 -0300

    Mostra o modaluserdetail  sem esperar retorno

--- components/CardUser/index.tsx
+++ components/CardUser/index.tsx
@@ -1,26 +1,62 @@
+import { useState } from 'react';
 import styles from '../../styles/components/CardUser.module.css'
+import ModalUserDetail from '../ModalUserDetail';
 
 interface CardUserProps {
     login: string,
-    urlAvatar: string,
-    setDadosUser: Function,
-    setModalUserDetail: Function,
+    urlAvatar: string
+}
+
+interface Member {
+    login: string,
+    name: string,
+    avatar_url: string,
+    public_repos: number,
+    followers: number,
+    created_at: Date,
 }
 
 const CardUser: React.FC<CardUserProps> = (props) => {
+    const [modalUserDetail, setModalUserDetail] = useState(false);
+    const [dadosUser, setDadosUser] = useState<Member>({
+        login: "",
+        name: "",
+        avatar_url: "",
+        public_repos: 0,
+        followers: 0,
+        created_at: new Date,
+    });
+
     function onClickUser() {
-        fetch(`https://api.github.com/users/${props.login}`)
-            .then(response => response.json())
-            .then(data => {
-                props.setDadosUser(data);
-                props.setModalUserDetail(true);
-            });
+        setModalUserDetail(true);
+        fetch(`https://api.github.com/users/${props.login}`).then(response => {
+            if (response.ok) {
+                return response.json().then(data => {
+                    setDadosUser(data);
+                });
+            } else {
+                alert('Erro na chamada')
+            }
+        }).catch(function (error) {
+            alert('Existe problema com a chamada Fetch:' + error.message);
+        });
     }
+
     return (
-        <li className={styles.rowGridMember} onClick={onClickUser}>
-            <img className={styles.imgProfile} src={props.urlAvatar} alt="avatar" />
-            <p>{props.login}</p>
-        </li>
+        <>
+            <li className={styles.rowGridMember} onClick={onClickUser}>
+                <img className={styles.imgProfile} src={props.urlAvatar} alt="avatar" />
+                <p>{props.login}</p>
+            </li>
+
+            {
+                modalUserDetail && < ModalUserDetail
+                    dadosUser={dadosUser}
+                    setModalUserDetail={setModalUserDetail}
+                    setDadosUser={setDadosUser}
+                />
+            }
+        </>
     )
 }
 
