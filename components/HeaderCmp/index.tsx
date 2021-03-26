c469e2d41b4f4358db38fa1a6604a3272e5b0b69
mndiaz90 <mayler900123@gmail.com>
2021-03-26 20:13:04 -0300

    Troca elemento div pelo header no cmp Header

--- components/HeaderCmp/index.tsx
+++ components/HeaderCmp/index.tsx
@@ -2,9 +2,9 @@ import styles from '../../styles/components/HeaderCmp.module.css'
 
 export function HeaderCmp() {
     return (
-        <div className={styles.containerHeader}>
+        <header className={styles.containerHeader}>
             <img className={styles.imgLogo} src="/logo.png" alt="logo" />
             <h1 style={{ color: 'var(--white)' }}>TESSERACT</h1>
-        </div>
+        </header>
     )
 }
